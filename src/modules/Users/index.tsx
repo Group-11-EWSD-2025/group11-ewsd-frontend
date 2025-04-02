4c05852e12e855e4f85e15284b7a4b5b1b6f56ba
lusivelin <linhtetpaing.dev@gmail.com>
2025-04-02 14:11:05 +0700

    refactor: add search params with api call for categories

--- src/modules/Users/index.tsx
+++ src/modules/Users/index.tsx
@@ -47,23 +47,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useGetRoles } from "../Auth/api/queryGetRoles";
 import { useDeleteUser } from "./api/mutateDeleteUser";
 import { useGetUsers } from "./api/queryGetUsers";
-
-// Custom hook for debounced value
-function useDebounce<T>(value: T, delay: number): T {
-  const [debouncedValue, setDebouncedValue] = React.useState<T>(value);
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [value, delay]);
-
-  return debouncedValue;
-}
+import { useDebounce } from "@/hooks/use-debounce";
 
 const Users = () => {
   // URL search params
